e8dafabc9e560eadfaac0e00320c430149cbbb58
Tiago Barros <tiagolp@msn.com>
2024-07-04 12:26:20 -0400

    Créer Controllers et Supprimer s de taxes - Supprimer le S taxes - Créer GroupesmotopropulseurController - Créer CarrosserieController

--- backend/app/Models/Taxe.php
+++ backend/app/Models/Taxe.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Taxes extends Model
+class Taxe extends Model
 {
     use HasFactory;
 
