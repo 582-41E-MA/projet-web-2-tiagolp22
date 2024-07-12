28db767143481378fc1468f09e6469a653dd4083
Matheus Rodrigues <matheus@hotmail.ca>
2024-07-11 21:29:24 -0400

    Ajout d'un carrousel sur la page d'accueil #171 - Intégration d'un carrousel utilisant react-slick pour présenter dynamiquement les voitures les plus récentes sur la page d'accueil. - Dans home Controller envoyer la variable de voitures qui contient la data - Adapter HomeController pour récupérer les voitures les plus récentes.

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-
+use App\Models\Voiture;
+use App\Models\Photo;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 class HomeController extends Controller
+
 {
     public function index()
     {
-        return Inertia::render('Home');
+
+
+        $voitures = Voiture::with('modele')
+            ->where('date_arrivee', '>=', Carbon::now()->subDays(30))
+            ->get();
+
+        foreach ($voitures as $voiture) {
+            $photo = Photo::where('voitures_id_voiture', $voiture->id_voiture)->first();
+            $voiture->photo_url = $photo ? asset(Storage::url($photo->photos)) : null;
+        }
+
+        return Inertia::render('Home', [
+            'voitures' => $voitures
+        ]);
     } 
 }
