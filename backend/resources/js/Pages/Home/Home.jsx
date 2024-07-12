1154b2bec5ad8eab8f61b052e22940cd425aa7ab
Matheus Rodrigues <matheus@hotmail.ca>
2024-07-11 21:29:24 -0400

    Ajout d'un carrousel sur la page d'accueil #171 - Intégration d'un carrousel utilisant react-slick pour présenter dynamiquement les voitures les plus récentes sur la page d'accueil. - Dans home Controller envoyer la variable de voitures qui contient la data - Adapter HomeController pour récupérer les voitures les plus récentes.

--- backend/resources/js/Pages/Home/Home.jsx
+++ backend/resources/js/Pages/Home/Home.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import Slider from 'react-slick';
 import { Link } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
-import { InertiaLink } from '@inertiajs/inertia-react';
 import './Home.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-function Home() {
-  const { t } = useTranslation();
+function Home({ voitures }) {
+  const { t, i18n } = useTranslation();
 
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4, 
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3, 
           slidesToScroll: 1,
           infinite: true,
           dots: true,
@@ -29,53 +28,49 @@ function Home() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2, // Mostra um carro por slide em telas menores que 600px
           slidesToScroll: 1,
         },
       },
     ],
   };
 
+  console.log(voitures);
+
   return (
     <>
       <Header />
       <div className="banner">
-            <img src="../../../img/banner/banner.jpg" alt="Image de fond du banner" className="banner-image" />
-            <div className="banner-content wrapper">
-                <h1>Les Meilleurs Voitures d'Occasion</h1>
-                <p>Découvrez des voitures d'occasion de qualité à des prix imbattables. Quebecar, transparence et service personnalisé pour votre confiance. </p>
-                  <Link href="/voitures" className="btn">{t('header.catalogue')}</Link>
-            </div>
-            <div className="banner-footer">
-                <p>Nous sommes fiers d'être Québécois</p>
-                <img src="../../../img/icones/flower.png" alt="flower qc" className="flower-qc" />
-            </div>
+        <img src="../../../img/banner/banner.jpg" alt="Image de fond du banner" className="banner-image" />
+        <div className="banner-content wrapper">
+          <h1>Les Meilleurs Voitures d'Occasion</h1>
+          <p>Découvrez des voitures d'occasion de qualité à des prix imbattables. Quebecar, transparence et service personnalisé pour votre confiance.</p>
+          <Link href="/voitures" className="btn">{t('header.catalogue')}</Link>
         </div>
-      <img className="banner"  alt="bannière" />
-      <div className="wrapper">
-        <div className="container">
+        <div className="banner-footer">
+          <p>Nous sommes fiers d'être Québécois</p>
+          <img src="../../../img/icones/flower.png" alt="flower qc" className="flower-qc" />
+        </div>
+      </div>
+     
+        <div className=".slider-container wrapper">
           <div className="carousel">
             <h2 className='Nouveautes'>{t('home.new_arrivals')}</h2>
             <Slider {...settings}>
-              <div className="slide">
-                <img src="https://via.placeholder.com/150" alt="Voiture" />
-                <h3>Voiture 1</h3>
-                <p>{t('home.car_description')}</p>
-              </div>
-              <div className="slide">
-                <img src="https://via.placeholder.com/150" alt="Voiture" />
-                <h3>Voiture 2</h3>
-                <p>{t('home.car_description')}</p>
-              </div>
-              <div className="slide">
-                <img src="https://via.placeholder.com/150" alt="Voiture" />
-                <h3>Voiture 3</h3>
-                <p>{t('home.car_description')}</p>
-              </div>
+              {voitures.map((voiture, index) => (
+                <div key={index} className="slide">
+                  <img src={voiture.photo_url ? voiture.photo_url : "../../../img/car/default_car.png"} alt={voiture.modele.nom_modele} />
+                  <h3>{voiture.annee} {voiture.modele.nom_modele}</h3>
+                  <p>{voiture.description ? JSON.parse(voiture.description)[i18n.language] : t('home.car_description')}</p>
+                  <Link href={`/voitures/${voiture.id_voiture}`} className="details-button">
+                    Plus d'info
+                  </Link>
+                </div>
+              ))}
             </Slider>
           </div>
         </div>
-      </div>
+      
       <Footer />
     </>
   );
