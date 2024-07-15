782ae35db21ae0c940e150b3533e09c7d27f62f0
Matheus Rodrigues <matheus@hotmail.ca>
2024-07-15 17:42:30 -0400

    fait la function pour supprimer user et modulariser btn actions

--- backend/resources/js/Pages/Pays/Pays.jsx
+++ backend/resources/js/Pages/Pays/Pays.jsx
@@ -26,7 +26,7 @@ const Pays = ({ pays }) => {
             <Header />
             <div className="pays-container">
                 <h1>{t('pays.list_title')}</h1>
-                <Link href="/pays/create" className="btn btn-primary">{t('pays.create_button')}</Link>
+                <Link href="/pays/create" className="create-button">{t('pays.create_button')}</Link>
                 <table className="table">
                     <thead>
                         <tr>
@@ -50,10 +50,10 @@ const Pays = ({ pays }) => {
                                     <td>{p.id_pays}</td>
                                     <td>{i18n.language === 'en' ? nomPays.en : nomPays.fr}</td>
                                     <td>
-                                        <Link href={`/pays/${p.id_pays}/edit`} className="btn btn-warning">{t('pays.edit')}</Link>
+                                        <Link href={`/pays/${p.id_pays}/edit`} className="edit-button">{t('pays.edit')}</Link>
                                         <button
                                             onClick={() => handleDelete(p.id_pays)}
-                                            className="btn btn-danger"
+                                            className="delete-button"
                                         >
                                             {t('pays.delete')}
                                         </button>
