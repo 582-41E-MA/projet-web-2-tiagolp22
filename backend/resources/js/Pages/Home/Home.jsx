4be3f431679dac6914eb867d0aa3f19ff9ef5aeb
Matheus Rodrigues <matheus@hotmail.ca>
2024-07-15 15:37:10 -0400

    Modif page Home Traduction des elements du banner Afficher le nom du constructeur dans carrousel

--- backend/resources/js/Pages/Home/Home.jsx
+++ backend/resources/js/Pages/Home/Home.jsx
@@ -43,33 +43,32 @@ function Home({ voitures }) {
       <div className="banner">
         <img src="../../../img/banner/banner.jpg" alt="Image de fond du banner" className="banner-image" />
         <div className="banner-content wrapper">
-          <h1>Les Meilleurs Voitures d'Occasion</h1>
-          <p>Découvrez des voitures d'occasion de qualité à des prix imbattables. Quebecar, transparence et service personnalisé pour votre confiance.</p>
+          <h1>{t('home.titre')}</h1>
+          <p>{t('home.description')}</p>
           <Link target="blank" href="/voitures" className="btn">{t('header.catalogue')}</Link>
         </div>
         <div className="banner-footer">
-          <p>Nous sommes fiers d'être Québécois</p>
+          <p>{t('home.fiers_quebecois')}</p>
           <img src="../../../img/icones/flower.png" alt="flower qc" className="flower-qc" />
         </div>
       </div>
 
-        <div className=".slider-container wrapper">
-          <div className="carousel">
-            <h2 className='Nouveautes'>{t('home.new_arrivals')}</h2>
-            <Slider {...settings}>
-              {voitures.map((voiture, index) => (
-                <div key={index} className="slide">
-                  <img src={voiture.photo_url ? voiture.photo_url : "../../../img/car/default_car.png"} alt={voiture.modele.nom_modele} />
-                  <h3>{voiture.annee} {voiture.modele.nom_modele}</h3>
-                  <p>{voiture.description ? JSON.parse(voiture.description)[i18n.language] : t('home.car_description')}</p>
-                  <Link target="blank" href={`/voitures/${voiture.id_voiture}`} className="details-button">
-                    Plus d'info
-                  </Link>
-                </div>
-              ))}
-            </Slider>
-          </div>
+      <div className="slider-container wrapper">
+        <div className="carousel">
+          <h2 className='Nouveautes'>{t('home.new_arrivals')}</h2>
+          <Slider {...settings}>
+            {voitures.map((voiture, index) => (
+              <div key={index} className="slide">
+                <img src={voiture.photo_url ? voiture.photo_url : "../../../img/car/default_car.png"} alt={voiture.modele.nom_modele} />
+                <h3>{voiture.annee} {voiture.modele.constructeur.nom_constructeur} {voiture.modele.nom_modele}</h3>
+                <Link target="blank" href={`/voitures/${voiture.id_voiture}`} className="details-button">
+                  {t('home.plus_info')}
+                </Link>
+              </div>
+            ))}
+          </Slider>
         </div>
+      </div>
 
       <Footer />
     </>
@@ -77,4 +76,3 @@ function Home({ voitures }) {
 }
 
 export default Home;
-
