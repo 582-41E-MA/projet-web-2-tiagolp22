a978dd51ef30f8b546173505bab3f5eee8565023
malek <malek_cheour@hotmail.com>
2024-07-16 14:38:04 -0400

    reparer l'affichage des photos apres l'application des filtres #172

--- backend/app/Http/Controllers/VoitureController.php
+++ backend/app/Http/Controllers/VoitureController.php
@@ -61,47 +61,51 @@ class VoitureController extends Controller
         $query = Voiture::query();
         $colors = ['White', 'Black', 'Red', 'Blue', 'Green', 'Yellow', 'Silver', 'Grey'];
         $modeles = Modele::pluck('nom_modele')->toArray();
-
         $constructeurs = Constructeur::pluck('nom_constructeur')->toArray();
-
+    
         if ($request->filled('etat')) {
             $query->where('etat_vehicule', $request->input('etat'));
         }
-
+    
         if ($request->filled('constructeur')) {
             $query->whereHas('modele.constructeur', function ($q) use ($request) {
                 $q->where('nom_constructeur', $request->input('constructeur'));
             });
         }
-
+    
         if ($request->filled('modele')) {
             $query->whereHas('modele', function ($q) use ($request) {
                 $q->where('nom_modele', $request->input('modele'));
             });
         }
-
+    
         if ($request->filled('annee')) {
             $query->where('annee', $request->input('annee'));
         }
-
+    
         if ($request->filled('prix_max')) {
             $query->where('prix_vente', '<=', $request->input('prix_max'));
         }
-
+    
         if ($request->filled('couleur')) {
             $query->whereJsonContains('couleur', $request->input('couleur'));
         }
-
+    
         if ($request->filled('nombre_places')) {
             $query->where('nombre_places', $request->input('nombre_places'));
         }
-
+    
         if ($request->filled('nombre_portes')) {
             $query->where('nombre_portes', $request->input('nombre_portes'));
         }
-
-        $voitures = $query->with('modele.constructeur')->get();
-
+    
+        $voitures = $query->with(['modele.constructeur', 'photos'])->get();
+    
+        foreach ($voitures as $voiture) {
+            $photo = $voiture->photos->first();
+            $voiture->photo_url = $photo ? asset(Storage::url($photo->photos)) : null;
+        }
+    
         return response()->json([
             'voitures' => $voitures,
             'filters' => [
@@ -111,6 +115,7 @@ class VoitureController extends Controller
             ],
         ]);
     }
+    
 
     public function store(VoitureRequest $request)
     {
