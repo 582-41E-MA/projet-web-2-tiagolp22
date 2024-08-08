f1d3f05ae20cac8176da7a31e000b296d7d0cf55
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-08-08 12:42:43 -0400

    Regler affichage des voitures page home
    - Mise en place d'un filtrage des voitures dans la méthode index du HomeController pour n'afficher que les véhicules postés dans les 30 derniers jours (nouveautés).
    - Filtrage des voitures en fonction de leur date d'arrivée, excluant celles réservées et ordonnant les résultats par date d'arrivée la plus récente.
    - Ajout d'une vérification pour inclure uniquement les voitures dont la date d'arrivée est inférieure ou égale à la date actuelle.

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -2,31 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+use Illuminate\Support\Facades\Log;
 use App\Models\Voiture;
 use App\Models\Photo;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
-class HomeController extends Controller
+use Illuminate\Support\Facades\DB;
 
+class HomeController extends Controller
 {
     public function index()
     {
-
-
+        $today = now()->toDateString();
+        $thirtyDaysAgo = now()->subDays(30)->toDateString();
+        $reservedCarIds = DB::table('reservations')->pluck('id_voiture')->toArray();
         $voitures = Voiture::with('modele')
-            ->where('date_arrivee', '>=', Carbon::now()->subDays(30))
+            ->where('date_arrivee', '<=', $today)
+            ->where('date_arrivee', '>=', $thirtyDaysAgo) // les nouvelles voitures
+            ->whereNotIn('id_voiture', $reservedCarIds)
+            ->orderBy('date_arrivee', 'desc')
             ->get();
 
+        $privilege_id = Auth::check() ? Auth::user()->privileges_id : null;
         foreach ($voitures as $voiture) {
             $photo = Photo::where('voitures_id_voiture', $voiture->id_voiture)->first();
             $voiture->photo_url = $photo ? asset(Storage::url($photo->photos)) : null;
         }
 
-        return Inertia::render('Home', [
-            'voitures' => $voitures
+        return Inertia::render('Home/Home', [
+            'voitures' => $voitures,
+            'privilege_id' => $privilege_id,
         ]);
-    } 
+    }
 }
